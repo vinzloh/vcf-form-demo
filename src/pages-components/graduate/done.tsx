bc54a3baccb9f2ac7c661f46226c7016f1f27fdf
Vince Loh <5501845+vinzloh@users.noreply.github.com>
2023-09-16 22:25:17 +0800

    swap new `ProgressCircle` component without `sass`

--- src/pages-components/graduate/done.tsx
+++ src/pages-components/graduate/done.tsx
@@ -1,10 +1,17 @@
-import { PieProgress } from '@/components/pie-progress';
+import * as React from 'react';
+
+import { ProgressCircle } from '@/components/progress-circle';
 import { $graduateRoute } from '@/store';
 
 import { Steps, type IStep } from './steps';
 import { StepsForm } from './steps-form';
 
 export function Done() {
+  const [percent, setPercent] = React.useState(0);
+  React.useEffect(() => {
+    setTimeout(() => setPercent(40), 800);
+  }, []);
+
   const steps: Array<IStep> = [
     { text: 'Personal Information' },
     { text: 'More Information' },
@@ -24,10 +31,10 @@ export function Done() {
       >
         <h1 className="text-2xl mb-1 text-center">Your Hireability Score</h1>
         <div className="my-2">
-          <PieProgress />
+          <ProgressCircle finalPercent={percent} />
         </div>
         <h2 className="text-base font-semibold">
-          Your profile is visible to Companies at 40% completion
+          Your profile is visible to Companies at {percent}% completion
         </h2>
         <div className="text-sm mb-4 text-center">
           The more you work on your profile the higher your score.
